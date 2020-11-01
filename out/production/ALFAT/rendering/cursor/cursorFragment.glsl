0eb5594d87e7833e0fcd398641be8d91f11ea319
Wilber Anderson <wilber.anderson17@gmail.com>
2020-11-01 14:59:07 -0500

    Remove autogenerated files.

--- out/production/ALFAT/rendering/cursor/cursorFragment.glsl
+++
@@ -1,9 +0,0 @@
-#version 330
-
-out vec4 out_color;
-uniform vec3 color;
-
-void main(void){
-	//Set the color to be the color loaded via uniform
-	out_color = vec4(color,1);
-}
--- out/production/ALFAT/rendering/cursor/cursorVertex.glsl
+++
@@ -1,9 +0,0 @@
-#version 330
-
-in vec2 position;
-uniform vec2 mousePosition;
-uniform float fontHeight;
-
-void main(void){
-    gl_Position = vec4(position.x+mousePosition.x, position.y*fontHeight+mousePosition.y, 1, 1);
-}
