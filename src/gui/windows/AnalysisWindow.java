982b445c222ee45369773de094f593968b20b33d
chloe-burdzel <chloe.burdzel@gmail.com>
2021-02-26 13:36:22 -0500

    Cancelling register search clears register highlighting

--- src/gui/windows/AnalysisWindow.java
+++ src/gui/windows/AnalysisWindow.java
@@ -55,6 +55,7 @@ public class AnalysisWindow extends GUIWindow{
     }
 
     public void onCancel(){
+        controller.getFlowchartWindowController().locateRegisters("");
         close();
     }
 
