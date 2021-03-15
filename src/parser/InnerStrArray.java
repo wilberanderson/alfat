a72d0c9b0d680de217cb73bf57310ed1c68c3780
boardskeytown <tmaloney@umassd.edu>
2021-03-15 13:23:45 -0400

    Added a separator field to the json and updated KeywordPatterns.java and CodeSyntax.java to account for this. Also started a possible schema to use be used in fixed field formatted parsing as seen in the MockFixedField.json

---
+++ src/parser/InnerStrArray.java
@@ -0,0 +1,20 @@
+package parser;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.ArrayList;
+
+public class InnerStrArray {
+    public ArrayList<String> inner;
+    @JsonCreator
+    public InnerStrArray(final ArrayList<String> inner) {
+        this.inner = inner;
+    }
+
+    @Override
+    public String toString() {
+        return "InnerStrArray{" +
+                "inner=" + inner +
+                '}';
+    }
+}
