98eb20a0bfb574d367886e8d574e1568dfa56b0d
Wilber Anderson <wilber.anderson17@gmail.com>
2021-04-16 20:32:13 -0400

    Updated shell script to open syntax file if supplied

--- scripts/alfat.sh
+++ scripts/alfat.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 #The path to the ALFAT jar file excluding ALFAT.jar
-FILEPATH="/Path/To/Jar/Not/Including/Jar/"
+FILEPATH="/absolute/path/to/ALFATjar/directory/"
 #Absolute path of the file that was passed
-FILE="$PWD/$1"
+FILE="$1"
+SYNTAX="$2"
 #Some errors may occur without changing the directory
 cd $FILEPATH
 if [ $# -eq 1 ]
   then
     #If there is one command line argument open with a file
     java -jar ALFAT.jar $FILE
+elif [ $# -eq 2 ]
+  then
+    #If there are two command line argument open with a file and syntax
+    java -jar ALFAT.jar -s $SYNTAX -f $FILE
 else
   #If there is not one argument open alfat with default options
   java -jar ALFAT.jar
